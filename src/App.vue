4fd006f9d1ec4936013e4e847db5c42e3a3ea126
Luna <luxureny@gmail.com>
2024-09-02 13:36:06 +0900

    7. 목록을 컴포넌트로 만들기

--- src/App.vue
+++ src/App.vue
@@ -3,29 +3,11 @@
   <Navbar />
   <!-- 속성명(props 변수="보낼 값" -->
   <Event :text="text"/>
-  <h1>영화정보</h1>
-  <!-- 반복문(배열의 자료명, 인덱스) -->
-  <div v-for="(movie, i) in data" :key="i" class="item">
-    <figure>
-      <img :src="`${movie.imgUrl}`" :alt="movie.title">
-    </figure>
-    <div class="info">
-    <!-- 데이터를 표시할 때 중괄호 두개 -->
-    <!-- 속성에 값을 바인딩할 때 앞에 콜론(:)을 붙여주면 됨 :속성명="데이터" -->
-      <h3 class="bg-yellow" :style="textRed">{{ movie.title }}</h3>
-      <p>개봉: {{ movie.year }}</p>
-      <p>장르: {{ movie.category }}</p>
-      <!-- v-on:이벤트명="실행코드, v-on 축약식: @로 대체 가능 -->
-      <!-- 코드가 짧을 경우 바로 추가 -->
-      <!-- <button @:click="like++">좋아요</button> <span>{{ like }}</span> -->
-      <!-- 버튼을 눌렀을 때 인덱스 값을 전달해야 함 -->
-      <button @:click="increaseLike(i)">좋아요</button>
-      <span>{{ movie.like }}</span>
-      <p>
-        <button @click="isModal=true; selectedMovie=i">상세보기</button>
-      </p>
-    </div>
-  </div>
+  <Movies
+      :data="data"
+      @openModal="isModal=true;selectedMovie=$event"
+      @increaseLike="increaseLike($event)"
+  />
   <!-- closeModal이벤트를 전달받아 상태 변경 -->
   <Modal
       :data="data"
@@ -43,6 +25,7 @@ import data from './assets/movies';
 import Navbar from "./components/Navbar.vue";
 import Modal from "./components/Modal.vue";
 import Event from "./components/Event.vue"; //이벤트 박스
+import Movies from "./components/Movies.vue";
 console.log(data);
 
 export default { // 안에 기능 정의
@@ -66,6 +49,7 @@ export default { // 안에 기능 정의
     Navbar: Navbar,
     Modal: Modal,
     Event: Event,
+    Movies: Movies,
   }
 }
 </script>
---
+++ src/components/Movies.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <h1>영화정보</h1>
+    <!-- 반복문(배열의 자료명, 인덱스) -->
+    <div v-for="(movie, i) in data" :key="i" class="item">
+      <figure>
+        <img :src="`${movie.imgUrl}`" :alt="movie.title">
+      </figure>
+      <div class="info">
+        <!-- 데이터를 표시할 때 중괄호 두개 -->
+        <!-- 속성에 값을 바인딩할 때 앞에 콜론(:)을 붙여주면 됨 :속성명="데이터" -->
+        <h3 class="bg-yellow" :style="textRed">{{ movie.title }}</h3>
+        <p>개봉: {{ movie.year }}</p>
+        <p>장르: {{ movie.category }}</p>
+        <!-- v-on:이벤트명="실행코드, v-on 축약식: @로 대체 가능 -->
+        <!-- 코드가 짧을 경우 바로 추가 -->
+        <!-- <button @:click="like++">좋아요</button> <span>{{ like }}</span> -->
+        <!-- 버튼을 눌렀을 때 인덱스 값을 전달해야 함 -->
+        <button @:click="$emit('increaseLike', i)">좋아요</button>
+        <span>{{ movie.like }}</span>
+        <p>
+          <button
+              @click="$emit('openModal', i)">상세보기</button>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "MoviesComponent",
+    props: {
+      data: Array,
+    }
+  }
+</script>
+
+<style>
+  .container {
+    padding: 20px;
+  }
+</style>
